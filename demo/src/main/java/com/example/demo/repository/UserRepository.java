c6b37633805721c9963b285cc5be79e17b511ce0
Kwon Oh-Jeong <kwon.ohejong@dreamsecurity.com>
2023-08-23 11:11:37 +0900

    2023-08-23 | simple demo 프로젝트 이름 001.r2dbc_with_h2db 로 변경

--- demo/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.modal.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface UserRepository extends ReactiveCrudRepository<User, Long> {
-}
